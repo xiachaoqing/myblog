f6c931ab0292aaf30720cf1adbb4cc569b6eccbf
夏朝清 <chaoqing.xia@banma-inc.com>
2020-11-16 21:27:38 +0800

    页面默认封面图

--- src/common/articleCard/articleCard.vue
+++ src/common/articleCard/articleCard.vue
@@ -40,7 +40,7 @@ export default {
   props: ['article'],
   data () {
     return {
-      defaultCover: 'http://blogimg.codebear.cn/FrTy2sZVtGZGYMFj6PAuNe7T6g3__water'
+      defaultCover: 'https://cdn.xiachaoqing.com/FrTy2sZVtGZGYMFj6PAuNe7T6g3__water.png'
     }
   },
   computed: {
